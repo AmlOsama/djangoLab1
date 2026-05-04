77ccae798579193f05f40ff9abd651f18cf59caa
Amal Osama <amlosama014@gmail.com>
2026-05-04 18:54:48 +0300

    Register Apps in Settings

--- ITIan/ITIan/settings.py
+++ ITIan/ITIan/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'course.apps.CourseConfig',
+    'trainee.apps.TraineeConfig',
 ]
 
 MIDDLEWARE = [
@@ -63,6 +65,7 @@ TEMPLATES = [
                 'django.contrib.messages.context_processors.messages',
             ],
         },
+        'DIRS':[BASE_DIR / 'templates'],
     },
 ]
 
@@ -115,3 +118,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [BASE_DIR /'static']
