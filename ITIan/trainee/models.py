9e7debfbc1b2160a0e23ce608deab05dfd419ac8
Amal Osama <amlosama014@gmail.com>
2026-05-04 19:04:44 +0300

    Add initial migration and model definitions for Trainee app
    - Created initial migration file for the Trainee model with fields: id, name, and email.
    - Added model definition in models.py for Trainee with appropriate field types.
    - Generated __pycache__ files for compiled Python files in the trainee and migrations directories.

--- ITIan/trainee/models.py
+++ ITIan/trainee/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 # Create your models here.
-class Trainee(models.Models):
+class Trainee(models.Model):
     name = models.CharField(max_length=100)
     email=models.EmailField()
 
