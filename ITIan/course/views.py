0edf986755b89bc0ed83b8533567c3ff32671d03
Amal Osama <amlosama014@gmail.com>
2026-05-04 19:20:28 +0300

    reate Views for course and trainee

--- ITIan/course/views.py
+++ ITIan/course/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render ,redirect, get_object_or_404
 
+from .models import Course
+
+def course_list(request):
+    courses =  Course.objects.all()
+    return render (request, 'course/course_list.html',{'courses':courses})
+
+
+def add_course(request):
+    if request.method == 'POST':
+        pass
+    return render (request,'course/add_course.html')
+
+
+def update_course(request, pk):
+    trainee = get_object_or_404(Course, pk=pk)
+    if request.method == 'POST':
+        # handle update
+        pass
+    return redirect('course_list')
+
+
+def delete_course(request, pk):
+    trainee = get_object_or_404(Course, pk=pk)
+    trainee.delete()
+    return redirect('course_list')
 # Create your views here.
+
+
+
--- ITIan/trainee/views.py
+++ ITIan/trainee/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render , redirect , get_object_or_404
+from .models import Trainee
 
+def trainee_list(request):
+    trainess =  Trainee.objects.all()
+    return render (request, 'trainee/trainee_list.html',{'trainees':trainess})
+
+
+def add_trainee(request):
+    if request.method == 'POST':
+        pass
+    return render (request,'trainee/add_trainee.html')
+
+
+def update_trainee(request, pk):
+    trainee = get_object_or_404(Trainee, pk=pk)
+    if request.method == 'POST':
+        # handle update
+        pass
+    return redirect('trainee_list')
+
+
+def delete_trainee(request, pk):
+    trainee = get_object_or_404(Trainee, pk=pk)
+    trainee.delete()
+    return redirect('trainee_list')
 # Create your views here.
