1ee8c5c95aab09569a985f13dea6caf5d29bdc0c
Amal Osama <amlosama014@gmail.com>
2026-05-04 19:55:47 +0300

    Add URL redirection for trainee list and update URL configurations

--- ITIan/ITIan/urls.py
+++ ITIan/ITIan/urls.py
@@ -15,12 +15,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path ,include
+from django.urls import path, include
 from django.contrib.auth import views as auth_views
+from django.shortcuts import redirect
+from . import views
 
 urlpatterns = [
+    path('', lambda request: redirect('trainee_list')),
     path('admin/', admin.site.urls),
-        path('trainees/', include('trainee.urls')),
+    path('trainees/', include('trainee.urls')),
     path('courses/', include('course.urls')),
     path('login/', auth_views.LoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
